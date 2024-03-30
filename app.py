9a79e41d1b8cf45e9dc3fc25d374291b0c627fbd
Souvik <98397984+Souvik34@users.noreply.github.com>
2024-03-30 14:44:10 +0530

    Update app.py

--- app.py
+++ app.py
@@ -40,7 +40,7 @@ def input_pdf_setup(uploaded_file):
 
 ## Streamlit App
 
-st.set_page_config(page_title="ATS Resume EXpert")
+st.set_page_config(page_title="ATS Resume System")
 st.header("ATS Tracking System")
 input_text=st.text_area("Job Description: ",key="input")
 uploaded_file=st.file_uploader("Upload your resume(PDF)...",type=["pdf"])
