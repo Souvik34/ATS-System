51e18b21a2c1b563f30b145278924e877b7a5e7b
Souvik <98397984+Souvik34@users.noreply.github.com>
2024-03-30 13:52:36 +0530

    Update app.py

--- app.py
+++ app.py
@@ -5,65 +5,56 @@ import base64
 import streamlit as st
 import os
 import io
-from PIL import Image
+from PIL import Image 
 import pdf2image
 import google.generativeai as genai
 
 genai.configure(api_key=os.getenv("GOOGLE_API_KEY"))
 
-def get_gemini_response(input, pdf_content,prompt):
+def get_gemini_response(input,pdf_cotent,prompt):
     model=genai.GenerativeModel('gemini-pro-vision')
-    response= model.generate_content([input,pdf_content[0],prompt])
+    response=model.generate_content([input,pdf_content[0],prompt])
     return response.text
 
 def input_pdf_setup(uploaded_file):
     if uploaded_file is not None:
-        ##convert pdf to image
+        ## Convert the PDF to image
         images=pdf2image.convert_from_bytes(uploaded_file.read())
 
         first_page=images[0]
 
-
-        ## convert to bytes
-
+        # Convert to bytes
         img_byte_arr = io.BytesIO()
-        first_page.save(img_byte_arr,format='JPEG')
+        first_page.save(img_byte_arr, format='JPEG')
         img_byte_arr = img_byte_arr.getvalue()
-
 
         pdf_parts = [
             {
                 "mime_type": "image/jpeg",
-                #encode to base64
-                "data": base64.b64encode(img_byte_arr).decode() 
+                "data": base64.b64encode(img_byte_arr).decode()  # encode to base64
             }
         ]
-
         return pdf_parts
     else:
         raise FileNotFoundError("No file uploaded")
-    
+
 ## Streamlit App
 
-
-st.set_page_config(page_title="ATS Resume Expert")
+st.set_page_config(page_title="ATS Resume EXpert")
 st.header("ATS Tracking System")
-
-input_text= st.text_area("Job Description: ", key="input")
-uploaded_file=st.file_uploader("Upload your resume(PDF)", type=["pdf"])
+input_text=st.text_area("Job Description: ",key="input")
+uploaded_file=st.file_uploader("Upload your resume(PDF)...",type=["pdf"])
 
 
 if uploaded_file is not None:
-    st.write("PDF uploaded successfully")
+    st.write("PDF Uploaded Successfully")
 
 
-submit1 = st.button("Tell me about my resume")
+submit1 = st.button("Tell Me About the Resume")
 
-submit2 = st.button("How can I improvise my skills")
+#submit2 = st.button("How Can I Improvise my Skills")
 
 submit3 = st.button("Percentage match")
-
-# input_prompts
 
 input_prompt1 = """
  You are an experienced Technical Human Resource Manager,your task is to review the provided resume against the job description. 
@@ -77,21 +68,20 @@ your task is to evaluate the resume against the provided job description. give m
 the job description. First the output should come as percentage and then keywords missing and last final thoughts.
 """
 
-
-if submit1: 
+if submit1:
     if uploaded_file is not None:
         pdf_content=input_pdf_setup(uploaded_file)
         response=get_gemini_response(input_prompt1,pdf_content,input_text)
-        st.subheader("The response is")
+        st.subheader("The Repsonse is")
         st.write(response)
     else:
-        st.write("Please upload the resume")
-    
+        st.write("Please uplaod the resume")
+
 elif submit3:
     if uploaded_file is not None:
         pdf_content=input_pdf_setup(uploaded_file)
         response=get_gemini_response(input_prompt3,pdf_content,input_text)
-        st.subheader("The response is")
+        st.subheader("The Repsonse is")
         st.write(response)
     else:
-        st.write("Please upload the resume")
+        st.write("Please uplaod the resume")
